27d60b592e7fa4d638e5401ca2464d73becef316
Mariano <marianomartin496@gmail.com>
2026-01-08 10:44:23 -0300

    fix: Remove duplicate Google Login button block

--- src/components/Auth/Signin/index.tsx
+++ src/components/Auth/Signin/index.tsx
@@ -171,19 +171,37 @@ const Signin = () => {
               </span>
 
               <div className="flex flex-col gap-4.5 mt-4.5">
-                {/* Google Login Button */}
-                <div className="w-full">
-                  <GoogleLogin
-                    onSuccess={handleGoogleSuccess}
-                    onError={handleGoogleError}
-                    useOneTap={false}
-                    text="signin_with"
-                    shape="rectangular"
-                    size="large"
-                    width="100%"
-                    theme="outline"
-                  />
+                {/* Google Login Button - Customized Visuals with Overlay */}
+                <div className="relative w-full">
+                  {/* Visual Button (Underlay) - Matches Microsoft Style */}
+                  <div className="flex w-full justify-center items-center gap-3.5 rounded-lg border border-gray-3 bg-gray-1 p-3 hover:bg-gray-2 cursor-pointer transition-colors">
+                    {/* Google Icon SVG */}
+                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M19.9895 10.1871C19.9895 9.36767 19.9214 8.76973 19.7842 8.14966H10.2042V11.848H15.8277C15.5916 13.0765 14.7945 14.5543 13.4139 15.4897L13.4199 15.5025L16.4352 17.8365L16.6439 17.8573C18.5746 16.0759 19.9895 13.4356 19.9895 10.1871Z" fill="#4285F4" />
+                      <path d="M10.2042 20.0003C12.9592 20.0003 15.2721 19.1114 16.9616 17.5531L13.7126 15.0152C12.7915 15.6329 11.6067 15.9877 10.2046 15.9877C7.54011 15.9877 5.24458 14.2078 4.43637 11.8081L4.41707 11.8252L1.28454 14.2464L1.27734 14.2657C2.98902 17.6593 6.38618 20.0003 10.2042 20.0003Z" fill="#34A853" />
+                      <path d="M4.43603 11.8083C4.21857 11.1713 4.09559 10.5182 4.09559 9.99986C4.09559 9.49399 4.21272 8.85292 4.42433 8.21588L4.41846 8.19736L1.31481 5.79517L1.27668 5.81665C0.537573 7.28821 0.117188 8.96677 0.117188 9.99986C0.117188 11.1897 0.559223 13.2505 1.40194 14.8396L4.43603 11.8083Z" fill="#FBBC05" />
+                      <path d="M10.2042 4.01185C12.1558 4.01185 13.4735 4.86591 14.2367 5.58414L17.0375 2.85406C15.2662 1.20261 12.9592 0 10.2042 0C6.38618 0 2.98902 2.34105 1.27734 5.81665L4.42466 8.21588C5.23326 5.80374 7.54011 4.01185 10.2042 4.01185Z" fill="#EB4335" />
+                    </svg>
+                    <span className="font-medium text-dark">Ingresa con Google</span>
+                  </div>
+
+                  {/* Functional Overlay - Invisible but Clickable */}
+                  <div className="absolute inset-0 opacity-0 z-10 overflow-hidden flex justify-center items-center">
+                    <div className="w-full h-full scale-[1.5] origin-center opacity-0 cursor-pointer">
+                      <GoogleLogin
+                        onSuccess={handleGoogleSuccess}
+                        onError={handleGoogleError}
+                        width="500" // Ensure it's wide enough to catch clicks anywhere
+                        theme="outline"
+                        size="large"
+                        text="signin_with"
+                        shape="rectangular"
+                      />
+                    </div>
+                  </div>
                 </div>
+
+
 
                 {/* Microsoft Login Button */}
                 <button
