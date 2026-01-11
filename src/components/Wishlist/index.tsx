7c2317927f30eef4087d398db492364736192703
Mariano <marianomartin496@gmail.com>
2026-01-10 22:16:15 -0300

    Fix texto ingles

--- src/components/Wishlist/index.tsx
+++ src/components/Wishlist/index.tsx
@@ -20,7 +20,7 @@ export const Wishlist: React.FC = () => {
       <section className="overflow-hidden py-20 bg-gray-2">
         <div className="max-w-[1170px] w-full mx-auto px-4 sm:px-8 xl:px-0">
           <div className="flex flex-wrap items-center justify-between gap-5 mb-7.5">
-            <h2 className="font-medium text-dark text-2xl">Your Wishlist</h2>
+            <h2 className="font-medium text-dark text-2xl">Tu Lista de Deseos </h2>
             <button className="text-blue" onClick={() => dispatch(clearWishlist())}>
               Clear Wishlist
             </button>
